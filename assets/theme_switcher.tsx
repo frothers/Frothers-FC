c5ae8e54b6bcaee2f94e15572d9f33f4f17d162e
Chris Chester <chrisjchester@gmail.com>
2024-02-18 21:25:30 +1300

    Theme switching

--- assets/theme_switcher.tsx
+++ assets/theme_switcher.tsx
@@ -19,7 +19,7 @@ function displayApp() {
 }
 
 function Toggle() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);  
 
   const toggleDarkMode = () => {
     document.body.classList.toggle("theme--default");
